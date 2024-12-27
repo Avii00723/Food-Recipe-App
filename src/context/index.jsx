5069597f596d4ea681f5f8293dd673633a808070
Avii00723 <105117218+Avii00723@users.noreply.github.com>
2024-12-27 10:23:25 +0530

    first commit

---
+++ src/context/index.jsx
@@ -0,0 +1,10 @@
+import {createContext} from "react";
+
+export const GlobalContext = createContext
+
+
+export default function GlobalState({children}){
+    return <GlobalContext.Provider value={{}}>
+        {children}
+    </GlobalContext.Provider>
+}
